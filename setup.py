98a668634bc8217e2c98df6584307a2326a0c2bc
Samuel Wise <samthemanwise@gmail.com>
2023-11-15 11:43:16 -0700

    added needed files and completes setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup 
+  
+setup( 
+    name='stat386_nba_package', 
+    version='0.1', 
+    description='Python package for Stat386 project on NBA regular and postseason player performance', 
+    author='Sam Wise/Matthew Ng', 
+    author_email='samthemanwise@gmail.com', 
+    packages=['stat386_nba_package'], 
+    install_requires=[ 
+        'numpy', 
+        'pandas',
+        'scikit-learn',
+        'matplotlib',
+        'seaborn'
+    ], 
+) 
