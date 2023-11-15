8d5cd4f188087cc24c840ec094728698cad8838c
Samuel Wise <samthemanwise@gmail.com>
2023-11-15 11:15:29 -0700

    tidied up clean_data.py

--- clean_data.py
+++ clean_data.py
@@ -1,40 +1,31 @@
-#Load in 2022-2023 Regular Season Data
-
-#Link: https://www.basketball-reference.com/leagues/NBA_2023_advanced.html
-#Hide the partial rows
-
 import pandas as pd
 
+# Load in 2022-2023 Regular Season Data
+# Link: https://www.basketball-reference.com/leagues/NBA_2023_advanced.html
+
 # Read the CSV file into a DataFrame
+
 reg22_23 = pd.read_csv("2022-2023reg.csv")
 
-reg22_23.head()
-
-#Clean Data
+# Clean Data
 
 reg22_23.drop(index = 0, inplace = True)
-
 columns_to_drop = ["Unnamed: 19", "Unnamed: 24", "Player-additional", "Rk"]
-
 reg22_23.drop(columns=columns_to_drop, axis=1, inplace=True)
 
-#Load in 2022-2023 Playoff Data
-
-#Link: https://www.basketball-reference.com/playoffs/NBA_2023_advanced.html
+# Load in 2022-2023 Playoff Data
+# Link: https://www.basketball-reference.com/playoffs/NBA_2023_advanced.html
 
 # Read the CSV file into a DataFrame
+
 playoff22_23 = pd.read_csv("2022-2023playoff.csv")
 
-playoff22_23.head()
-
-#Clean Data
+# Clean Data
 
 columns_to_drop = ["Unnamed: 19", "Unnamed: 24", "Player-additional", "Rk"]
-
 playoff22_23.drop(columns=columns_to_drop, axis=1, inplace=True)
 
 #Write clean data to new csv 
 
-reg22_23.to_csv("clean_reg22_23", index = False)
-
-playoff22_23.to_csv("clean_playoff22_23", index = False)
+reg22_23.to_csv("clean_reg22_23.csv", index = False)
+playoff22_23.to_csv("clean_playoff22_23.csv", index = False)
