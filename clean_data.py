623e1a58b43dd08ffeb541c8534534da7057c1ef
Matthew Ng <mattng23@byu.edu>
2023-11-20 11:45:51 -0700

    Changed seasons to 2021-2022

--- clean_data.py
+++ clean_data.py
@@ -1,34 +1,34 @@
 import pandas as pd
 
 # Load in 2022-2023 Regular Season Data
-# Link: https://www.basketball-reference.com/leagues/NBA_2023_advanced.html
+# Link: https://www.basketball-reference.com/leagues/NBA_2022_advanced.html
 
 # Read the CSV file into a DataFrame
 
-reg22_23 = pd.read_csv("2022-2023reg.csv")
+reg21_22 = pd.read_csv("2021-2022reg.csv")
 
 # Clean Data
 
-reg22_23.drop(index = 0, inplace = True)
+reg21_22.drop(index = 0, inplace = True)
 columns_to_drop = ["Unnamed: 19", "Unnamed: 24", "Player-additional", "Rk"]
-reg22_23.drop(columns=columns_to_drop, axis=1, inplace=True)
+reg21_22.drop(columns=columns_to_drop, axis=1, inplace=True)
 
 # Load in 2022-2023 Playoff Data
-# Link: https://www.basketball-reference.com/playoffs/NBA_2023_advanced.html
+# Link: https://www.basketball-reference.com/playoffs/NBA_2022_advanced.html
 
 # Read the CSV file into a DataFrame
 
-playoff22_23 = pd.read_csv("2022-2023playoff.csv")
+playoff21_22 = pd.read_csv("2021-2022playoff.csv")
 
 # Clean Data
 
 columns_to_drop = ["Unnamed: 19", "Unnamed: 24", "Player-additional", "Rk"]
-playoff22_23.drop(columns=columns_to_drop, axis=1, inplace=True)
+playoff21_22.drop(columns=columns_to_drop, axis=1, inplace=True)
 
 #Write clean data to new csv 
 
-reg22_23.to_csv("clean_reg22_23.csv", index = False)
-playoff22_23.to_csv("clean_playoff22_23.csv", index = False)
+reg21_22.to_csv("clean_reg22_23.csv", index = False)
+playoff21_22.to_csv("clean_playoff22_23.csv", index = False)
 
 
 # Read in RAPTOR Dataframe
