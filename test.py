b2f8bba1b803efdebdf5f5ed52ecb47c823992b2
Matthew Ng <mattng23@byu.edu>
2023-11-28 16:34:12 -0700

    Add Sources for metrics

--- test.py
+++ test.py
@@ -1,2 +1,18 @@
 #Metrics To Consider
 ## VORP, RAPTOR, PREDATOR, WAR
+
+#### Sources
+
+## VORP
+
+# https://www.sportskeeda.com/basketball/what-vorp-mean-nba-exploring-advanced-stat-determines-player-s-impact
+
+## Raptor
+
+# https://fivethirtyeight.com/features/introducing-raptor-our-new-metric-for-the-modern-nba/
+# https://fivethirtyeight.com/features/how-our-raptor-metric-works/
+
+## Predator and WAR
+
+# https://fivethirtyeight.com/features/how-our-raptor-metric-works/
+
