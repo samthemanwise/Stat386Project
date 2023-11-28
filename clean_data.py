0cfb0874e5dce2d9bf77c6d270aa453b23a379a7
Matthew Ng <mattng23@byu.edu>
2023-11-28 16:40:23 -0700

    Change years on df

--- clean_data.py
+++ clean_data.py
@@ -36,16 +36,37 @@ playoff21_22.to_csv("clean_playoff21_22.csv", index = False)
 # Read in RAPTOR Dataframe
 # Link: https://github.com/fivethirtyeight/data/blob/master/nba-raptor/modern_RAPTOR_by_team.csv
 
+raptor_df = pd.read_csv("modern_RAPTOR_by_team.csv")
+
+#Clean Data
+
+# Specify the columns you want to keep
+columns_to_keep = ["player_name","season","season_type","team","poss","mp","raptor_offense","raptor_total",
+                   "war_total","war_reg_season","war_playoffs","predator_offense","predator_total"]
+
+# Keep only the specified columns
+raptor_df = raptor_df[columns_to_keep]
+
+raptor_df.head()
+
+#Get Just 2021-2022 Season
+
+raptor_df_2022 = raptor_df[raptor_df["season"] == 2022]
+
+raptor_df_2022
+
+## Create Master CSV
+
 # Renaming 'vorp' in data frames so they can join
 reg21_22 = reg21_22.rename(columns={'VORP': 'vorp_reg'})
 
 playoff21_22 = playoff21_22.rename(columns={'VORP': 'vorp_post'})
 
 # changing name of player column in raptor to join
-raptor_df_2023 = raptor_df_2023.rename(columns={'player_name': 'Player', 'Tm': 'team'})
+raptor_df_2022 = raptor_df_2022.rename(columns={'player_name': 'Player', 'Tm': 'team'})
 
 # Performing the joins
-result = pd.merge(raptor_df_2023, reg21_22, on='Player', how='inner')
+result = pd.merge(raptor_df_2022, reg21_22, on='Player', how='inner')
 result = pd.merge(result, playoff21_22, on='Player', how='inner')
 
 columns_to_keep = ['Player', 'season', 'season_type', 'team', 'raptor_total', 'war_total', 'predator_total', 'vorp_reg', 'vorp_post']
