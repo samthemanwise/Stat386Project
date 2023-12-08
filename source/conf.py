2a474350194660edcd43a485a4896f373ab07a11
Matthew Ng <mattng23@byu.edu>
2023-12-08 11:32:27 -0700

    Add extensions

--- source/conf.py
+++ source/conf.py
@@ -20,7 +20,12 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.githubpages",
+]
+
 
 templates_path = ['_templates']
 exclude_patterns = []
