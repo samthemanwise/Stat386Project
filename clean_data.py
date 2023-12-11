4872ca98657677f2c1d88e5c7cdcb31fcfbd05ef
Samuel Wise <samthemanwise@gmail.com>
2023-12-11 16:18:44 -0700

    made new modules and cleaned up demo

--- clean_data.py
+++ clean_data.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from unidecode import unidecode
 
 # Load in 2022-2023 Regular Season Data
 # Link: https://www.basketball-reference.com/leagues/NBA_2022_advanced.html
@@ -28,51 +27,7 @@ playoff21_22 = pd.read_csv("2021-2022playoff.csv")
 columns_to_drop = ["Unnamed: 19", "Unnamed: 24", "Player-additional", "Rk"]
 playoff21_22.drop(columns=columns_to_drop, axis=1, inplace=True)
 
-reg21_22['Player'] = reg21_22['Player'].apply(unidecode)
-playoff21_22['Player'] = playoff21_22['Player'].apply(unidecode)
-
 #Write clean data to new csv 
 
 reg21_22.to_csv("clean_reg21_22.csv", index = False)
 playoff21_22.to_csv("clean_playoff21_22.csv", index = False)
-
-
-# Read in RAPTOR Dataframe
-# Link: https://github.com/fivethirtyeight/data/blob/master/nba-raptor/modern_RAPTOR_by_team.csv
-
-raptor_df = pd.read_csv("modern_RAPTOR_by_team.csv")
-
-#Clean Data
-
-# Specify the columns you want to keep
-columns_to_keep = ["player_name","season","season_type","team","poss","mp","raptor_offense","raptor_total",
-                   "war_total","war_reg_season","war_playoffs","predator_offense","predator_total"]
-
-# Keep only the specified columns
-raptor_df = raptor_df[columns_to_keep]
-
-raptor_df.head()
-
-#Get Just 2021-2022 Season
-
-raptor_df_2022 = raptor_df[raptor_df["season"] == 2022]
-
-raptor_df_2022
-
-## Create Master CSV
-
-# Renaming 'vorp' in data frames so they can join
-reg21_22 = reg21_22.rename(columns={'VORP': 'vorp_reg'})
-
-playoff21_22 = playoff21_22.rename(columns={'VORP': 'vorp_post'})
-
-# changing name of player column in raptor to join
-raptor_df_2022 = raptor_df_2022.rename(columns={'player_name': 'Player', 'Tm': 'team'})
-
-# Performing the joins
-result = pd.merge(raptor_df_2022, reg21_22, on='Player', how='inner')
-result = pd.merge(result, playoff21_22, on='Player', how='inner')
-
-columns_to_keep = ['Player', 'season', 'season_type', 'team', 'raptor_total', 'war_total', 'predator_total', 'vorp_reg', 'vorp_post']
-result = result[columns_to_keep]
-result.to_csv("master.csv", index = False)
